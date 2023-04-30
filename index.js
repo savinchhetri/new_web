175e84fa1999a64323144fc25b63d683f614cbea
savinchhetri <1kingsabin@gmail.com>
2023-04-30 19:31:32 +0545

    changes

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("helo world !")
+console.log("hi")
+age = 20
+active = false
+course = "web dev"
+console.log("age is",age)
+console.log("active is",active)
+console.log("course is",course)
+console.log(active)
